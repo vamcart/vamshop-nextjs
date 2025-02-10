9c8e15e5710378eba343e333faa35febb7b9ef2e
VamCart <vamcart@gmail.com>
2025-02-10 17:38:37 +0300

    Корректировка кода

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -2,6 +2,8 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prismadb = globalForPrisma.prisma || new PrismaClient();
+const prismadb = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismadb;
+
+export default prismadb;
