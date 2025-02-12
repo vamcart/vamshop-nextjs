663d8272dbf7de1ea38b9577db366e81fc1b1786
VamCart <vamcart@gmail.com>
2025-02-12 11:25:18 +0300

    Добавлена страница размеры

---
+++ app/api/[storeId]/sizes/[sizeId]/route.ts
@@ -0,0 +1,123 @@
+import { NextResponse } from "next/server";
+
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { sizeId: string } }
+) {
+  try {
+    const paramsData = await params;
+    if (!paramsData.sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+
+    const size = await prismadb.size.findUnique({
+      where: {
+        id: paramsData.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { sizeId: string; storeId: string } }
+) {
+  try {
+    const paramsData = await params;
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!paramsData.sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: paramsData.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const size = await prismadb.size.delete({
+      where: {
+        id: paramsData.sizeId,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { sizeId: string; storeId: string } }
+) {
+  try {
+    const paramsData = await params;
+    const { userId } = await auth();
+
+    const body = await req.json();
+
+    const { name, value } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!value) {
+      return new NextResponse("Value is required", { status: 400 });
+    }
+
+    if (!paramsData.sizeId) {
+      return new NextResponse("Size id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: paramsData.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 405 });
+    }
+
+    const size = await prismadb.size.update({
+      where: {
+        id: paramsData.sizeId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(size);
+  } catch (error) {
+    console.log("[SIZE_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
